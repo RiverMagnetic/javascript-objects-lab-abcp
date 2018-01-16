9349e2f92cf9e86cc207b48247553ec9d541ddcc
River Magnetic <magneticcontinuum@gmail.com>
2018-01-16 15:37:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 
-var object = { key: 'bar' }
+
 function deleteFromObjectByKey(object, key) {
  var newObj = Object.assign({}, object)
 delete newObj.key
